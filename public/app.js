93c942c614612720108fd538a78e4074db5d3b88
Lyle Mitchell <ljwmitchell@gmail.com>
2018-06-12 20:43:30 +0100

    adds flyto method

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 var app = function() {
-  
+
 }
 
 const drawMap = function () {
@@ -8,11 +8,15 @@ const drawMap = function () {
   const javastreet = [55.801507, -4.003805];
   const pyongyang =[39.03385, 125.75432];
   const zoomLevel = 10;
-  const mainMap = new MapWrapper(mapDiv, pyongyang, zoomLevel);
-  mainMap.addMarker(pyongyang);
+  const mainMap = new MapWrapper(mapDiv, glasgow, zoomLevel);
+  mainMap.addMarker(glasgow);
 
+
+const button = document.querySelector('#take-me');
+   button.addEventListener('click', function(){
+       mainMap.moveTo(pyongyang)
+   });
 }
-
 
 
 window.addEventListener("load", drawMap);
